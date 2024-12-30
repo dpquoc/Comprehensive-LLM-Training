026d81e4bdad9eecca59fa7c3fc1631e972d2d7b
dpquoc <117615204+dpquoc@users.noreply.github.com>
2024-12-30 15:47:14 +0700

    update

--- SFT/utils.py
+++ SFT/utils.py
@@ -19,6 +19,8 @@ import pandas as pd
 
 from typing import Dict, Optional, List
 from peft import LoraConfig
+from typing import Optional, Union, Callable
+
 
 
 DEFAULT_CHATML_CHAT_TEMPLATE = "{% for message in messages %}\n{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% if loop.last and add_generation_prompt %}{{'<|im_start|>assistant\n' }}{% endif %}{% endfor %}"
