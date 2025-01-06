360de9b4e30fb78b88b5d529314056e0dfe012c2
dpquoc <dangpquoc2022@gmail.com>
2025-01-06 22:18:49 +0700

    update

--- SeqClassification/utils.py
+++ SeqClassification/utils.py
@@ -98,8 +98,7 @@ def simple_preprocess(
 # <|im_end|>
 # <|im_start|>assistant"""
 
-    template = """<|START_OF_TURN_TOKEN|><|SYSTEM_TOKEN|>You are a helpful assistant. Your task is to read the following prompt carefully. Compare the two responses provided and determine which response better.<|END_OF_TURN_TOKEN|><|START_OF_TURN_TOKEN|><|USER_TOKEN|>
-Prompt: {prompt}
+    template = """<|START_OF_TURN_TOKEN|><|SYSTEM_TOKEN|>You are a helpful assistant. Your task is to read the following prompt carefully. Compare the two responses provided and determine which response better.<|END_OF_TURN_TOKEN|><|START_OF_TURN_TOKEN|><|USER_TOKEN|>Prompt: {prompt}
 
 Response A: {response_a}
 
