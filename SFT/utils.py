f72c8013fd9a85417c87f090ca7b3b41e2c0c3a4
dpquoc <dangpquoc2022@gmail.com>
2025-02-02 00:54:35 +0700

    fix bugs

--- SFT/utils.py
+++ SFT/utils.py
@@ -177,12 +177,12 @@ def preprocess(
                 raise NotImplementedError
             target += _target
 
-        print("\nFinal lengths before assertion:")
-        print(f"input_id length: {len(input_id)}")
-        print(f"target length: {len(target)}")
-        print(f"Full final decoded input: {tokenizer.decode(input_id)}")
+        # print("\nFinal lengths before assertion:")
+        # print(f"input_id length: {len(input_id)}")
+        # print(f"target length: {len(target)}")
+        # print(f"Full final decoded input: {tokenizer.decode(input_id)}")
 
-        break
+        # break
         assert len(input_id) == len(target)
         input_id += [tokenizer.pad_token_id] * (max_len - len(input_id))
         target += [IGNORE_TOKEN_ID] * (max_len - len(target))
